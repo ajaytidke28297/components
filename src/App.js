b30829f157db7349c12fde9a12bbaeed7266ab15
Ajay Tidke <ajaytidke28297@gmail.com>
2023-02-16 13:14:38 +0530

    add Modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import Sidebar from './components/Sidebar';
-import Route from './components/Route';
-import AccordionPage from './pages/AccordionPage';
-import DropdownPage from './pages/DropdownPage';
-import ButtonPage from './pages/ButtonPage';
+import Route from "./components/Route";
+import Sidebar from "./components/Sidebar";
+import AccordionPage from "./pages/AccordionPage";
+import ButtonPage from "./pages/ButtonPage";
+import DropdownPage from "./pages/DropdownPage";
+import ModalPage from "./pages/ModalPage";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route path="/buttons">
           <ButtonPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
         </Route>
       </div>
     </div>
